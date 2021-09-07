0d301e8b9e3191259646e8c7e778a0760b6175b5
Renan Torres <pesterenan@gmail.com>
2021-09-07 15:56:25 -0300

    Estado and Cidade entities added

--- src/main/java/com/pesterenan/cursomc/CursomcApplication.java
+++ src/main/java/com/pesterenan/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.pesterenan.cursomc.domain.Categoria;
+import com.pesterenan.cursomc.domain.Cidade;
+import com.pesterenan.cursomc.domain.Estado;
 import com.pesterenan.cursomc.domain.Produto;
 import com.pesterenan.cursomc.repositories.CategoriaRepository;
+import com.pesterenan.cursomc.repositories.CidadeRepository;
+import com.pesterenan.cursomc.repositories.EstadoRepository;
 import com.pesterenan.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,7 +23,11 @@ public class CursomcApplication implements CommandLineRunner {
 	private CategoriaRepository catRepo;
 	@Autowired
 	private ProdutoRepository prodRepo;
-	
+	@Autowired
+	private EstadoRepository estRepo;
+	@Autowired
+	private CidadeRepository cidRepo;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -31,15 +39,30 @@ public class CursomcApplication implements CommandLineRunner {
 		Produto p1 = new Produto(null, "Computador", 2000);
 		Produto p2 = new Produto(null, "Impressora", 800);
 		Produto p3 = new Produto(null, "Mouse", 80);
-		
-		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategoria().addAll(Arrays.asList(cat1));
-		p2.getCategoria().addAll(Arrays.asList(cat1,cat2));
+		p2.getCategoria().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategoria().addAll(Arrays.asList(cat1));
-		catRepo.saveAll(Arrays.asList(cat1,cat2));
-		prodRepo.saveAll(Arrays.asList(p1,p2,p3));
+
+		catRepo.saveAll(Arrays.asList(cat1, cat2));
+		prodRepo.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estRepo.saveAll(Arrays.asList(est1, est2));
+		cidRepo.saveAll(Arrays.asList(c1, c2, c3));
+
 	}
 
 }
---
+++ src/main/java/com/pesterenan/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.pesterenan.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pesterenan.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Long> {
+
+}
