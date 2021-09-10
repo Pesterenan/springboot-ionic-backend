9c8f041b5a6bca303ca902f4d88726eab032d9c7
Renan Torres <pesterenan@gmail.com>
2021-09-10 19:22:15 -0300

    Customizing the validation return to the user

--- src/main/java/com/pesterenan/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/pesterenan/cursomc/dto/CategoriaDTO.java
@@ -11,11 +11,10 @@ import com.pesterenan.cursomc.domain.Categoria;
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	
 	private Long id;
-	
-	@NotEmpty(message="Preenchimento obrigatório.")
-	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres.")
+
+	@NotEmpty(message = "Preenchimento obrigatório.")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres.")
 	private String nome;
 
 	public CategoriaDTO() {
