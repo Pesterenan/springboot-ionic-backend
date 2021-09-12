848e1e9df0539deba44622d3752107c1da537377
Renan Torres <pesterenan@gmail.com>
2021-09-11 21:14:07 -0300

    PUT, DELETE and GET for the Cliente class.

--- src/main/java/com/pesterenan/cursomc/resources/ClienteResource.java
+++ src/main/java/com/pesterenan/cursomc/resources/ClienteResource.java
@@ -1,29 +1,71 @@
 package com.pesterenan.cursomc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pesterenan.cursomc.domain.Cliente;
+import com.pesterenan.cursomc.dto.ClienteDTO;
 import com.pesterenan.cursomc.services.ClienteService;
+import com.pesterenan.cursomc.services.exceptions.DataIntegrityException;
 import com.pesterenan.cursomc.services.exceptions.ObjectNotFoundException;
 
-
 @RestController
-@RequestMapping(value="/clientes")
+@RequestMapping(value = "/clientes")
 public class ClienteResource {
-	
+
 	@Autowired
 	private ClienteService cliService;
 
-	@GetMapping(value= "/{id}")
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<Cliente> listar(@PathVariable Long id) throws ObjectNotFoundException {
 		Cliente obj = cliService.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
-	
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Long id) {
+		Cliente obj = cliService.fromDTO(objDto);
+		obj.setId(id);
+		obj = cliService.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) throws DataIntegrityException {
+		cliService.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping()
+	public ResponseEntity<List<ClienteDTO>> findAll() throws ObjectNotFoundException {
+		List<Cliente> list = cliService.findAll();
+		List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+
+	// Requisição de objetos Cliente por Página, para evitar trazer todos os
+	// registros de uma vez só.
+	@GetMapping(value = "/page")
+	public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy) throws ObjectNotFoundException {
+		Page<Cliente> list = cliService.findPage(page, linesPerPage, direction, orderBy);
+		Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok().body(listDTO);
+	}
 }
--- src/main/java/com/pesterenan/cursomc/services/ClienteService.java
+++ src/main/java/com/pesterenan/cursomc/services/ClienteService.java
@@ -1,24 +1,66 @@
 package com.pesterenan.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.pesterenan.cursomc.domain.Cliente;
+import com.pesterenan.cursomc.dto.ClienteDTO;
 import com.pesterenan.cursomc.repositories.ClienteRepository;
+import com.pesterenan.cursomc.services.exceptions.DataIntegrityException;
 import com.pesterenan.cursomc.services.exceptions.ObjectNotFoundException;
-
 
 @Service
 public class ClienteService {
 
 	@Autowired
 	private ClienteRepository cliRepo;
-	
+
 	public Cliente find(Long id) throws ObjectNotFoundException {
 		Optional<Cliente> obj = cliRepo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + " Tipo: " + Cliente.class.getName()));
+	}
+
+	// Cria um novo objetoCliente e atualiza somente os dados necessários pra depois salvar no banco de dados.
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj,obj);
+		return cliRepo.save(newObj);
+	}
+
+	// Atualiza dados de nome e email do novo objeto cliente para poder manter outros dados imutáveis.
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public void deleteById(Long id) {
+		find(id);
+		try {
+			cliRepo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um porque há entidades relacionadas.");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		List<Cliente> list = cliRepo.findAll();
+		return list;
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String direction, String orderBy) {
+		PageRequest pageReq = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return cliRepo.findAll(pageReq);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
 }
