abfe357ff804c01afa00f32c19d1fbbd742cf673
Renan Torres <pesterenan@gmail.com>
2021-09-11 21:58:42 -0300

    Inserting a new Cliente with POST

--- src/main/java/com/pesterenan/cursomc/resources/ClienteResource.java
+++ src/main/java/com/pesterenan/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.pesterenan.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,14 +12,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.pesterenan.cursomc.domain.Categoria;
 import com.pesterenan.cursomc.domain.Cliente;
 import com.pesterenan.cursomc.dto.ClienteDTO;
+import com.pesterenan.cursomc.dto.ClienteNewDTO;
 import com.pesterenan.cursomc.services.ClienteService;
 import com.pesterenan.cursomc.services.exceptions.DataIntegrityException;
 import com.pesterenan.cursomc.services.exceptions.ObjectNotFoundException;
@@ -34,6 +39,17 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> listar(@PathVariable Long id) throws ObjectNotFoundException {
 		Cliente obj = cliService.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = cliService.fromDTO(objDto);
+		obj = cliService.insert(obj);
+		// Boa prática de REST: Retornar URI do objeto criado após inserção
+		// Pega o URI do request atual, e adiciona a nova id do novo objeto já no banco
+		// de dados.
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 	@PutMapping(value = "/{id}")
--- src/main/java/com/pesterenan/cursomc/services/ClienteService.java
+++ src/main/java/com/pesterenan/cursomc/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.pesterenan.cursomc.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.pesterenan.cursomc.domain.Cidade;
 import com.pesterenan.cursomc.domain.Cliente;
+import com.pesterenan.cursomc.domain.Endereco;
+import com.pesterenan.cursomc.domain.enums.TipoCliente;
 import com.pesterenan.cursomc.dto.ClienteDTO;
+import com.pesterenan.cursomc.dto.ClienteNewDTO;
 import com.pesterenan.cursomc.repositories.ClienteRepository;
+import com.pesterenan.cursomc.repositories.EnderecoRepository;
 import com.pesterenan.cursomc.services.exceptions.DataIntegrityException;
 import com.pesterenan.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -21,6 +28,8 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository cliRepo;
+	@Autowired
+	private EnderecoRepository endRepo;
 
 	public Cliente find(Long id) throws ObjectNotFoundException {
 		Optional<Cliente> obj = cliRepo.findById(id);
@@ -28,14 +37,24 @@ public class ClienteService {
 				"Objeto não encontrado! Id: " + id + " Tipo: " + Cliente.class.getName()));
 	}
 
-	// Cria um novo objetoCliente e atualiza somente os dados necessários pra depois salvar no banco de dados.
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = cliRepo.save(obj);
+		endRepo.saveAll(obj.getEnderecos());
+		return obj;
+	}
+
+	// Cria um novo objetoCliente e atualiza somente os dados necessários pra depois
+	// salvar no banco de dados.
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
-		updateData(newObj,obj);
+		updateData(newObj, obj);
 		return cliRepo.save(newObj);
 	}
 
-	// Atualiza dados de nome e email do novo objeto cliente para poder manter outros dados imutáveis.
+	// Atualiza dados de nome e email do novo objeto cliente para poder manter
+	// outros dados imutáveis.
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
@@ -62,5 +81,22 @@ public class ClienteService {
 
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 }
