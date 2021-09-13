10ce045c5d8a84d53a52f7ecece2c14a36513f81
Renan Torres <pesterenan@gmail.com>
2021-09-12 23:47:28 -0300

    Custoim validation: Email already existing on Cliente updating

--- src/main/java/com/pesterenan/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/pesterenan/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.pesterenan.cursomc.domain.Cliente;
+import com.pesterenan.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
